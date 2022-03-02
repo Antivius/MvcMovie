3f0a3c178e6eb52421a5b7c88d655a5bd1398916
PetraNoppari <99483924+PetraNoppari@users.noreply.github.com>
2022-03-02 17:00:27 +0200

    yritys luoda tyhja branch

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public String Index()
+        {
+            return "This is my default action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+
+        public String Welcome()
+        {
+            return "This is welcome action method...";
+        }
+
+    }
+}
